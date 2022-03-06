a84aed7037ebefbc390d703d20721bd718c96831
Samantha <51409073+Tari38@users.noreply.github.com>
2022-03-06 15:16:53 +0000

    minimising css and add engine to json

--- src/components/challenges/qrcode/qr-code.jsx
+++ src/components/challenges/qrcode/qr-code.jsx
@@ -1,12 +1,12 @@
-import "./styles.css";
-import { Button, Container, Card } from "reactstrap";
+import "./qr-code.css";
+import { Button, Container, Card, NavLink } from "reactstrap";
 import qrcode from "./images/image-qr-code.png";
 
 export default function QrCode() {
   return (
     <>
       <Card className="qrcode">
-        <Container className="container">
+        <Container className="qr-container">
           <img src={qrcode} alt="qr code" id="qr"></img>
           <h2>Improve your front-end skills by building projects</h2>
           <p>
@@ -14,10 +14,11 @@ export default function QrCode() {
             skills to the next level
           </p>
         </Container>
-				</Card>
+				<br />
 				<div>
-					<Button href="https://github.com/Tari38/frontend-mentor-qr-code-challenge">View my Code</Button>
+					<NavLink href="https://github.com/Tari38/frontend-mentor-qr-code-challenge"><Button id="qr-button" >View my Code</Button></NavLink>
 				</div>
+				</Card>
     </>
   );
 }
